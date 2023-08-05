fa077d770631e369f9c764e6d13a3ed021bc1d10
jameydev <jybryce4@gmail.com>
2023-08-05 09:32:22 -0500

    CharCounter component example added

---
+++ src/components/CharCounter.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export default function CharCounter() {
+  const [userInput, setUserInput] = useState('');
+
+  function inputHandler(event) {
+    setUserInput(event.target.value);
+  };
+
+  const numChars = userInput.length;
+
+  return (
+    <>
+      <input type="text" onChange={inputHandler} />
+      <p>Characters entered: {numChars}</p>
+    </>
+  );
+};
